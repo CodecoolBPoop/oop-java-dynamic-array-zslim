a3b1e85aa35a13c3bc65eb44023e8c6002efc252
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-11-29 09:48:09 +0100

    implemented add & remove

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,5 +1,7 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.Arrays;
+
 // put your code here!
 public class DynamicIntArray {
     private int[] dataArray;
@@ -14,6 +16,45 @@ public class DynamicIntArray {
     }
     @Override
     public String toString() {
-        return "Implement me plz.. Pretty plz..";
+        StringBuilder arrayString = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            arrayString.append(" ");
+            arrayString.append(dataArray[i]);
+        }
+        return arrayString.toString();
+    }
+    public int[] get() {
+        return dataArray;
+    }
+    public void add(int number) {
+        int[] newArray = new int[this.length + 1];
+        System.arraycopy(dataArray, 0, newArray, 0, length);
+        newArray[length] = number;
+        dataArray = newArray;
+        length++;
+    }
+    public void remove(int numberToDrop) {
+        int[] newArray = new int[length];
+        int nextNewArrayIndex = 0;
+        int nextSectionStart = 0;
+        for (int i = 0; i < length; i++) {
+            if (dataArray[i] == numberToDrop) {
+                System.arraycopy(dataArray, nextSectionStart, newArray, nextNewArrayIndex, i - nextSectionStart);
+                nextNewArrayIndex += i - nextSectionStart;
+                nextSectionStart = i + 1;
+            }
+        }
+        System.arraycopy(dataArray, nextSectionStart, newArray, nextNewArrayIndex, length - nextSectionStart);
+        nextNewArrayIndex += length - nextSectionStart;
+        int[] result = Arrays.copyOfRange(newArray, 0, nextNewArrayIndex);
+        if (nextSectionStart == 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        this.dataArray = result;
+        length = this.dataArray.length;
+    }
+    public void insert(int place, int number) {
+        // check for positive place
+        // if place is too large just add number
     }
 }
