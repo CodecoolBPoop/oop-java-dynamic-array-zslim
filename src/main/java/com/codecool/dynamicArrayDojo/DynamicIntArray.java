254a4721741121e4f87f4f283fefe41b9b224321
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-11-29 09:46:59 +0100

    created constructors & implemented toString

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,6 +2,16 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    private int[] dataArray;
+    public int length;
+    public DynamicIntArray(int initialSize) {
+        dataArray = new int[initialSize];
+        this.length = initialSize;
+    }
+    public DynamicIntArray() {
+        dataArray = new int[0];
+        length = 0;
+    }
     @Override
     public String toString() {
         return "Implement me plz.. Pretty plz..";
